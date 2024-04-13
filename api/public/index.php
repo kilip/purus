d6811119af92d36086b0acf9ed1b1e18e3e5adf8
Anthonius Munthi <me@itstoni.com>
2024-04-13 22:23:51 +0800

    wip

--- api/public/index.php
+++ api/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the purus project.
+ * This file is part of the Purus project.
  *
  * (c) Anthonius Munthi <me@itstoni.com>
  *
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-use App\Kernel;
+use Purus\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
